5802e439c96857bfc893ce17809dd185fccd846b
Sangram Reddy <reddy.horcrux@gmail.com>
2020-11-29 19:05:35 +0000

    Updated tests

--- app/tests/SearchService/utils/Constants.ts
+++ app/tests/SearchService/utils/Constants.ts
@@ -6,8 +6,8 @@ export class Constants {
   static REVIEWS = "reviews"
 
   static VALIDATION_GIG_ID = "Ypow63QBFqowVtPehyM3"
-  static VALIDATION_PROFILE_ID = "4bb6a530-6dcb-4202-b022-ce60048a883d"
-  static VALIDATION_REVIEW_ID = "Y5oy63QBFqowVtPeiSPh"
+  static VALIDATION_PROFILE_ID = "R75kFXYB2LWFzatCXg6L"
+  static VALIDATION_REVIEW_ID = "Dr5VFXYB2LWFzatCpA4u"
 
   static INDEX = "INDEX"
   static DOCTYPE = "DOCTYPE"
